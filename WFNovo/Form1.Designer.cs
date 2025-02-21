57faff6d7ac3012ede2bbe3d58154f07f27b88b1
Mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2025-02-21 19:24:42 -0300

    Adicionar arquivos de projeto.

---
+++ WFNovo/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WFNovo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblUserName = new Label();
+            lblPassword = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Location = new Point(118, 77);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(65, 15);
+            lblUserName.TabIndex = 0;
+            lblUserName.Text = "User Name";
+            lblUserName.Click += label1_Click;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(118, 111);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(57, 15);
+            lblPassword.TabIndex = 1;
+            lblPassword.Text = "Password";
+            lblPassword.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(189, 74);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(205, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(189, 108);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(204, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(227, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 27);
+            button1.TabIndex = 4;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(488, 401);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUserName);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUserName;
+        private Label lblPassword;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
